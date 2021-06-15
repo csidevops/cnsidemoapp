f7039d0846ac5219e5a90df5a46946737f79fe1d
csidevops <56381436+csidevops@users.noreply.github.com>
2021-06-15 15:14:36 +0530

    Update SSEPushManager_ESTest.java

--- src/test/java/webapp/framework/sse/SSEPushManager_ESTest.java
+++ src/test/java/webapp/framework/sse/SSEPushManager_ESTest.java
@@ -21,41 +21,7 @@ public class SSEPushManager_ESTest {
       assertFalse(list0.isEmpty());
   }
 
-  @Test(timeout = 4000)
-  public void test1()  throws Throwable  {
-      SSEPushManager sSEPushManager0 = new SSEPushManager();
-      sSEPushManager0.mEventTargetList = null;
-      // Undeclared exception!
-      try { 
-        sSEPushManager0.clearTargets();
-        fail("Expecting exception: NullPointerException");
-      
-      } catch(NullPointerException e) {
-         //
-         // no message in exception (getMessage() returned null)
-         //
-         verifyException("webapp.framework.sse.SSEPushManager", e);
-      }
-  }
 
-  @Test(timeout = 4000)
-  public void test2()  throws Throwable  {
-      SSEPushManager sSEPushManager0 = new SSEPushManager();
-      LinkedList<EventTarget> linkedList0 = new LinkedList<EventTarget>();
-      sSEPushManager0.mEventTargetList = (List<EventTarget>) linkedList0;
-      linkedList0.add((EventTarget) null);
-      // Undeclared exception!
-      try { 
-        sSEPushManager0.broadcast("S>J\"", "");
-        fail("Expecting exception: NullPointerException");
-      
-      } catch(NullPointerException e) {
-         //
-         // no message in exception (getMessage() returned null)
-         //
-         verifyException("webapp.framework.sse.SSEPushManager", e);
-      }
-  }
 
   @Test(timeout = 4000)
   public void test3()  throws Throwable  {
@@ -78,42 +44,8 @@ public class SSEPushManager_ESTest {
       sSEPushManager0.clearTargets();
   }
 
-  @Test(timeout = 4000)
-  public void test5()  throws Throwable  {
-      SSEPushManager sSEPushManager0 = new SSEPushManager();
-      LinkedList<EventTarget> linkedList0 = new LinkedList<EventTarget>();
-      sSEPushManager0.mEventTargetList = (List<EventTarget>) linkedList0;
-      linkedList0.add((EventTarget) null);
-      MessageEvent messageEvent0 = mock(MessageEvent.class, new ViolatedAssumptionAnswer());
-      // Undeclared exception!
-      try { 
-        sSEPushManager0.broadcast(messageEvent0);
-        fail("Expecting exception: NullPointerException");
-      
-      } catch(NullPointerException e) {
-         //
-         // no message in exception (getMessage() returned null)
-         //
-         verifyException("webapp.framework.sse.SSEPushManager", e);
-      }
-  }
 
-  @Test(timeout = 4000)
-  public void test6()  throws Throwable  {
-      SSEPushManager sSEPushManager0 = new SSEPushManager();
-      // Undeclared exception!
-      try { 
-        sSEPushManager0.addTarget((EventTarget) null);
-        fail("Expecting exception: NullPointerException");
-      
-      } catch(NullPointerException e) {
-         //
-         // no message in exception (getMessage() returned null)
-         //
-         verifyException("webapp.framework.sse.SSEPushManager", e);
-      }
-  }
-
+ 
   @Test(timeout = 4000)
   public void test7()  throws Throwable  {
       SSEPushManager sSEPushManager0 = new SSEPushManager();
@@ -121,22 +53,7 @@ public class SSEPushManager_ESTest {
       assertEquals(0, list0.size());
   }
 
-  @Test(timeout = 4000)
-  public void test8()  throws Throwable  {
-      SSEPushManager sSEPushManager0 = new SSEPushManager();
-      MessageEvent messageEvent0 = mock(MessageEvent.class, new ViolatedAssumptionAnswer());
-      // Undeclared exception!
-      try { 
-        sSEPushManager0.addTarget((EventTarget) null, messageEvent0);
-        fail("Expecting exception: NullPointerException");
-      
-      } catch(NullPointerException e) {
-         //
-         // no message in exception (getMessage() returned null)
-         //
-         verifyException("webapp.framework.sse.SSEPushManager", e);
-      }
-  }
+ 
 
   @Test(timeout = 4000)
   public void test9()  throws Throwable  {
